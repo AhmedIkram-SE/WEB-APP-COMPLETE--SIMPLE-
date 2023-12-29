fb4966f0d95a5fd401159c07da17069b268faaf4
Ahmed Ikram <ahmedikram2003@gmail.com>
2023-12-29 23:37:50 +0500

    Fixed Web Error

--- Backend/index.js
+++ Backend/index.js
@@ -9,19 +9,13 @@ const {
   deleteStudent,
   updateStudent,
 } = require("./StudentsOperations");
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // or specify a specific origin
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
-// app.use(
-//   cors({
-//     origin: ["https://web-app-complete-frontend.vercel.app"],
-//     methods: ["GET,POST,PUT,DELETE"],
-//   })
-// );
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // or specify a specific origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 let database = mongoose.connect(
